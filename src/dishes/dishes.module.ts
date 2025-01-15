c3c5b9bb5c5b574ac054217a75c0f5ea11e9ce5d
Yefrosynii Kolenko <contact@yefro.dev>
2025-01-15 20:29:05 +0200

    refactor: move dish methods to base @ folder

--- src/dishes/dishes.module.ts
+++ src/dishes/dishes.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
 import { DrizzleModule } from "@postgress-db/drizzle.module";
 
-import { DishesController } from "./dishes.controller";
-import { DishesService } from "./dishes.service";
+import { DishesController } from "./@/dishes.controller";
+import { DishesService } from "./@/dishes.service";
 
 @Module({
   imports: [DrizzleModule],
