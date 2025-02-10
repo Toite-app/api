e3f73938cf3a43925996fbe7a0dc101a66c3b33d
Yefrosynii Kolenko <contact@yefro.dev>
2025-02-10 17:37:00 +0200

    feat: sending data to users about subscription update

--- src/orders/@queue/services/orders-socket-notifier.service.ts
+++ src/orders/@queue/services/orders-socket-notifier.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { plainToClass } from "class-transformer";
 import { SocketService } from "src/@socket/socket.service";
-import { GatewayWorker } from "src/@socket/socket.types";
+import {
+  GatewayClient,
+  SocketEventType,
+  SocketOrderUpdateEvent,
+} from "src/@socket/socket.types";
 import { OrderEntity } from "src/orders/@/entities/order.entity";
-
-export enum OrderSocketEvents {
-  SUBSCRIPTION_UPDATED = "subscription:update",
-}
 
 @Injectable()
 export class OrdersSocketNotifier {
@@ -20,28 +20,41 @@ export class OrdersSocketNotifier {
    * @param order
    */
   public async handle(order: OrderEntity) {
-    const workers = await this.socketService.getWorkers();
+    const clients = await this.socketService.getClients();
     const subscriptions = await this.socketService.getSubscriptions();
 
-    const orderSubscriptions = subscriptions.filter((subscription) => {
-      return (
-        subscription.type === "ORDER" && subscription.data.orderId === order.id
-      );
+    const clientsMap = new Map<string, GatewayClient>(
+      clients.map((client) => [client.clientId, client]),
+    );
+
+    const messages: {
+      recipient: GatewayClient;
+      event: string;
+      data: any;
+    }[] = [];
+
+    subscriptions.forEach((subscription) => {
+      if (
+        subscription.type === "ORDER" &&
+        subscription.data.orderId === order.id
+      ) {
+        const recipient = clientsMap.get(subscription.clientId);
+        if (!recipient) return;
+
+        messages.push({
+          recipient,
+          event: SocketEventType.SUBSCRIPTION_UPDATE,
+          data: {
+            id: subscription.id,
+            type: "ORDER",
+            order: plainToClass(OrderEntity, order, {
+              excludeExtraneousValues: true,
+            }),
+          } satisfies SocketOrderUpdateEvent,
+        });
+      }
     });
 
-    const clientIds = orderSubscriptions.map(
-      (subscription) => subscription.clientId,
-    );
-
-    await this.socketService.emitTo(
-      {
-        clientIds,
-        workerIds: undefined,
-      },
-      OrderSocketEvents.SUBSCRIPTION_UPDATED,
-      {
-        type: "ORDER",
-      },
-    );
+    await this.socketService.emit(messages);
   }
 }
