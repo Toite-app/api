495476dc5b8137a7885e76204b486de2c6fd8056
Yefrosynii Kolenko <contact@yefro.dev>
2025-02-24 14:43:19 +0200

    fix: swagger entity for order dish

--- src/orders/@/entities/order-dish-modifier.entity.ts
+++ src/orders/@/entities/order-dish-modifier.entity.ts
@@ -7,6 +7,7 @@ export class OrderDishModifierEntity {
   @Expose()
   @ApiProperty({
     description: "Unique identifier of the order dish modifier",
+    example: "d290f1ee-6c54-4b01-90e6-d701748f0851",
   })
   id: string;
 
@@ -14,6 +15,7 @@ export class OrderDishModifierEntity {
   @Expose()
   @ApiProperty({
     description: "Name of the order dish modifier",
+    example: "Extra Cheese",
   })
   name: string;
 }
