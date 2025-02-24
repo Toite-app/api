71f562b50a946fd802b4f949c25bebb5688c5516
Yefrosynii Kolenko <contact@yefro.dev>
2025-02-24 14:43:19 +0200

    fix: swagger entity for order dish

--- src/orders/@/entities/order-dish.entity.ts
+++ src/orders/@/entities/order-dish.entity.ts
@@ -84,7 +84,7 @@ export class OrderDishEntity implements IOrderDish {
   @Type(() => OrderDishModifierEntity)
   @ApiProperty({
     description: "Dish modifiers",
-    example: ["d290f1ee-6c54-4b01-90e6-d701748f0851"],
+    type: [OrderDishModifierEntity],
   })
   modifiers: OrderDishModifierEntity[];
 
