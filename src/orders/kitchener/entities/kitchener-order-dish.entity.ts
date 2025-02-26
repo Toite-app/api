a55ed63cf9ceeb55273ce13366d372bfbd0c118b
Yefrosynii Kolenko <contact@yefro.dev>
2025-02-26 08:56:58 +0200

    feat: fetching workshops for order dishes

--- src/orders/kitchener/entities/kitchener-order-dish.entity.ts
+++ src/orders/kitchener/entities/kitchener-order-dish.entity.ts
@@ -1,5 +1,33 @@
-import { PickType } from "@nestjs/swagger";
+import { IsBoolean, IsString, IsUUID } from "@i18n-class-validator";
+import { ApiProperty, PickType } from "@nestjs/swagger";
+import { Expose, Type } from "class-transformer";
 import { OrderDishEntity } from "src/orders/@/entities/order-dish.entity";
+
+export class KitchenerOrderDishWorkshopEntity {
+  @Expose()
+  @IsUUID()
+  @ApiProperty({
+    description: "Workshop id",
+    example: "123e4567-e89b-12d3-a456-426614174000",
+  })
+  id: string;
+
+  @Expose()
+  @IsString()
+  @ApiProperty({
+    description: "Workshop name",
+    example: "Kitchen",
+  })
+  name: string;
+
+  @Expose()
+  @IsBoolean()
+  @ApiProperty({
+    description: "Is my workshop",
+    example: false,
+  })
+  isMyWorkshop: boolean;
+}
 
 export class KitchenerOrderDishEntity extends PickType(OrderDishEntity, [
   "id",
@@ -8,4 +36,12 @@ export class KitchenerOrderDishEntity extends PickType(OrderDishEntity, [
   "quantity",
   "quantityReturned",
   "isAdditional",
-]) {}
+]) {
+  @Expose()
+  @ApiProperty({
+    description: "Workshops",
+    type: [KitchenerOrderDishWorkshopEntity],
+  })
+  @Type(() => KitchenerOrderDishWorkshopEntity)
+  workshops: KitchenerOrderDishWorkshopEntity[];
+}
