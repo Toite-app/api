10d849282c0dd86c56e6a343cc55ed3de94cc6f6
Yefrosynii Kolenko <contact@yefro.dev>
2025-03-06 16:31:10 +0200

    feat: create and update methods for dishes-menus controller

--- src/dishes-menus/dishes-menus.controller.ts
+++ src/dishes-menus/dishes-menus.controller.ts
@@ -2,10 +2,12 @@ import { Controller } from "@core/decorators/controller.decorator";
 import { Serializable } from "@core/decorators/serializable.decorator";
 import { Worker } from "@core/decorators/worker.decorator";
 import { RequestWorker } from "@core/interfaces/request";
-import { Get } from "@nestjs/common";
+import { Body, Get, Param, Patch, Post } from "@nestjs/common";
 import { ApiOkResponse, ApiOperation } from "@nestjs/swagger";
 import { EnableAuditLog } from "src/@base/audit-logs/decorators/audit-logs.decorator";
 import { DishesMenusService } from "src/dishes-menus/dishes-menus.service";
+import { CreateDishesMenuDto } from "src/dishes-menus/dto/create-dishes-menu.dto";
+import { UpdateDishesMenuDto } from "src/dishes-menus/dto/update-dishes-menu.dto";
 import { DishesMenuEntity } from "src/dishes-menus/entity/dishes-menu.entity";
 
 @Controller("dishes-menus")
@@ -24,6 +26,43 @@ export class DishesMenusController {
   })
   async findAll(@Worker() worker: RequestWorker) {
     return this.dishesMenusService.findMany({
+      worker,
+    });
+  }
+
+  @EnableAuditLog()
+  @Post()
+  @Serializable(DishesMenuEntity)
+  @ApiOperation({
+    summary: "Creates a new dish menu",
+  })
+  @ApiOkResponse({
+    description: "Dish menu has been successfully created",
+  })
+  async create(
+    @Worker() worker: RequestWorker,
+    @Body() payload: CreateDishesMenuDto,
+  ) {
+    return this.dishesMenusService.create(payload, {
+      worker,
+    });
+  }
+
+  @EnableAuditLog()
+  @Patch(":dishesMenuId")
+  @Serializable(DishesMenuEntity)
+  @ApiOperation({
+    summary: "Updates a dish menu",
+  })
+  @ApiOkResponse({
+    description: "Dish menu has been successfully updated",
+  })
+  async update(
+    @Worker() worker: RequestWorker,
+    @Param("dishesMenuId") dishesMenuId: string,
+    @Body() payload: UpdateDishesMenuDto,
+  ) {
+    return this.dishesMenusService.update(dishesMenuId, payload, {
       worker,
     });
   }
--- src/dishes-menus/dishes-menus.service.ts
+++ src/dishes-menus/dishes-menus.service.ts
@@ -1,10 +1,19 @@
+import { BadRequestException } from "@core/errors/exceptions/bad-request.exception";
+import { ForbiddenException } from "@core/errors/exceptions/forbidden.exception";
+import { NotFoundException } from "@core/errors/exceptions/not-found.exception";
+import { ServerErrorException } from "@core/errors/exceptions/server-error.exception";
 import { RequestWorker } from "@core/interfaces/request";
 import { Inject, Injectable } from "@nestjs/common";
 import { schema } from "@postgress-db/drizzle.module";
-import { dishesMenus } from "@postgress-db/schema/dishes-menus";
+import {
+  dishesMenus,
+  dishesMenusToRestaurants,
+} from "@postgress-db/schema/dishes-menus";
 import { and, eq, inArray, SQL } from "drizzle-orm";
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import { PG_CONNECTION } from "src/constants";
+import { CreateDishesMenuDto } from "src/dishes-menus/dto/create-dishes-menu.dto";
+import { UpdateDishesMenuDto } from "src/dishes-menus/dto/update-dishes-menu.dto";
 import { DishesMenuEntity } from "src/dishes-menus/entity/dishes-menu.entity";
 
 @Injectable()
@@ -13,6 +22,70 @@ export class DishesMenusService {
     @Inject(PG_CONNECTION) private readonly pg: NodePgDatabase<typeof schema>,
   ) {}
 
+  /**
+   * Fetches a dish menu by its ID
+   * @param id - The ID of the dish menu
+   * @param options - Options for fetching the menu
+   * @param options.worker - The worker making the request
+   * @returns The fetched dish menu
+   */
+  public async findOne(
+    id: string,
+    options: {
+      worker: RequestWorker;
+    },
+  ): Promise<DishesMenuEntity> {
+    const { worker } = options;
+
+    const fetchedMenu = await this.pg.query.dishesMenus.findFirst({
+      where: and(
+        eq(dishesMenus.id, id),
+        // For owner only their own menus
+        worker.role === "OWNER"
+          ? eq(dishesMenus.ownerId, worker.id)
+          : undefined,
+        // For admin only menus of their restaurants
+        worker.role === "ADMIN"
+          ? inArray(
+              dishesMenus.ownerId,
+              worker.workersToRestaurants.map(
+                ({ restaurantId }) => restaurantId,
+              ),
+            )
+          : undefined,
+      ),
+      with: {
+        dishesMenusToRestaurants: {
+          columns: {},
+          with: {
+            restaurant: {
+              columns: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!fetchedMenu) {
+      throw new NotFoundException("errors.dishes-menus.dish-menu-not-found");
+    }
+
+    return {
+      ...fetchedMenu,
+      restaurants: fetchedMenu.dishesMenusToRestaurants.map(
+        ({ restaurant }) => restaurant,
+      ),
+    };
+  }
+  /**
+   * Fetches all dish menus
+   * @param options - Options for fetching the menus
+   * @param options.worker - The worker making the request
+   * @returns All dish menus
+   */
   public async findMany(options: {
     worker: RequestWorker;
   }): Promise<DishesMenuEntity[]> {
@@ -56,5 +129,194 @@ export class DishesMenusService {
       ...dishesMenu,
       restaurants: dishesMenusToRestaurants.map(({ restaurant }) => restaurant),
     }));
+  }
+
+  private async validateRestaurants(ownerId: string, restaurantIds: string[]) {
+    if (restaurantIds.length === 0) {
+      return true;
+    }
+
+    const restaurants = await this.pg.query.restaurants.findMany({
+      where: (restaurants, { inArray, and, eq }) =>
+        and(
+          inArray(restaurants.id, restaurantIds),
+          eq(restaurants.ownerId, ownerId),
+        ),
+      columns: {
+        id: true,
+      },
+    });
+
+    if (restaurants.length !== restaurantIds.length) {
+      throw new BadRequestException(
+        "errors.dishes-menus.some-restaurant-are-not-owned-by-the-owner",
+      );
+    }
+  }
+
+  /**
+   * Creates a new dish menu
+   * @param options - Options for creating the menu
+   * @param options.worker - The worker making the request
+   * @returns The created dish menu
+   */
+  public async create(
+    payload: CreateDishesMenuDto,
+    options: {
+      worker: RequestWorker;
+    },
+  ): Promise<DishesMenuEntity> {
+    const { worker } = options;
+
+    const { restaurantIds, ...menuData } = payload;
+
+    let ownerId: string | null = null;
+
+    if (worker.role === "OWNER") {
+      // For owner we will auto-assign it's id to menu
+      ownerId = worker.id;
+    } else if (
+      worker.role === "SYSTEM_ADMIN" ||
+      worker.role === "CHIEF_ADMIN"
+    ) {
+      // SYSTEM and CHIEF admins should provide ownerId
+      if (!payload.ownerId) {
+        throw new BadRequestException(
+          "errors.dishes-menus.owner-id-is-required",
+        );
+      }
+
+      ownerId = payload.ownerId;
+    } else {
+      throw new ForbiddenException("errors.dishes-menus.not-enough-rights");
+    }
+
+    if (typeof ownerId !== "string") {
+      throw new ServerErrorException();
+    }
+
+    // Validate restaurants
+    await this.validateRestaurants(ownerId, restaurantIds);
+
+    const menuId = await this.pg.transaction(async (tx) => {
+      const [createdMenu] = await tx
+        .insert(dishesMenus)
+        .values({
+          ...menuData,
+          ownerId: String(ownerId),
+        })
+        .returning({
+          id: dishesMenus.id,
+        });
+
+      await tx.insert(dishesMenusToRestaurants).values(
+        restaurantIds.map((restaurantId) => ({
+          dishesMenuId: createdMenu.id,
+          restaurantId,
+        })),
+      );
+
+      return createdMenu.id;
+    });
+
+    return this.findOne(menuId, {
+      worker,
+    });
+  }
+
+  /**
+   * Updates a dish menu
+   * @param id - The ID of the dish menu
+   * @param payload - The payload for updating the menu
+   * @param options - Options for updating the menu
+   * @param options.worker - The worker making the request
+   * @returns The updated dish menu
+   */
+  public async update(
+    id: string,
+    payload: UpdateDishesMenuDto,
+    options: {
+      worker: RequestWorker;
+    },
+  ) {
+    const { worker } = options;
+
+    const { restaurantIds, ...menuData } = payload;
+
+    const menu = await this.pg.query.dishesMenus.findFirst({
+      where: eq(dishesMenus.id, id),
+      columns: {
+        ownerId: true,
+      },
+      with: {
+        dishesMenusToRestaurants: {
+          columns: {
+            restaurantId: true,
+          },
+        },
+      },
+    });
+
+    if (!menu) {
+      throw new NotFoundException("errors.dishes-menus.dish-menu-not-found");
+    }
+
+    if (worker.role === "OWNER" && menu.ownerId !== worker.id) {
+      // Owners can only update their own menus
+      throw new ForbiddenException("errors.dishes-menus.not-enough-rights");
+    } else if (
+      // Admins can only update menus that have their restaurants
+      worker.role === "ADMIN" &&
+      !worker.workersToRestaurants.some(
+        ({ restaurantId }) => restaurantId === menu.ownerId,
+      )
+    ) {
+      throw new ForbiddenException("errors.dishes-menus.not-enough-rights");
+    } else if (
+      worker.role !== "SYSTEM_ADMIN" &&
+      worker.role !== "CHIEF_ADMIN"
+    ) {
+      // If not a OWNER, or ADMIN, or SYSTEM_ADMIN, or CHIEF_ADMIN, then you can't update the menu
+      throw new ForbiddenException("errors.dishes-menus.not-enough-rights");
+    }
+
+    const ownerId =
+      (worker.role === "SYSTEM_ADMIN" || worker.role === "CHIEF_ADMIN") &&
+      payload.ownerId
+        ? payload.ownerId
+        : menu.ownerId;
+
+    if (restaurantIds) {
+      // Validate restaurants
+      await this.validateRestaurants(ownerId, restaurantIds);
+    }
+
+    // Make update operation
+    await this.pg.transaction(async (tx) => {
+      await tx
+        .update(dishesMenus)
+        .set({
+          ...menuData,
+          ownerId: String(ownerId),
+        })
+        .where(eq(dishesMenus.id, id));
+
+      if (restaurantIds) {
+        await tx
+          .delete(dishesMenusToRestaurants)
+          .where(eq(dishesMenusToRestaurants.dishesMenuId, id));
+
+        await tx.insert(dishesMenusToRestaurants).values(
+          restaurantIds.map((restaurantId) => ({
+            dishesMenuId: id,
+            restaurantId,
+          })),
+        );
+      }
+    });
+
+    return this.findOne(id, {
+      worker,
+    });
   }
 }
