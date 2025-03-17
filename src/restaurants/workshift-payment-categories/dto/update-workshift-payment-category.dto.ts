591e173f9b331ff3803583aa80c29af4b0064459
Yefrosynii Kolenko <contact@yefro.dev>
2025-03-17 18:40:46 +0200

    feat: updating is active state

--- src/restaurants/workshift-payment-categories/dto/update-workshift-payment-category.dto.ts
+++ src/restaurants/workshift-payment-categories/dto/update-workshift-payment-category.dto.ts
@@ -2,5 +2,5 @@ import { PartialType, PickType } from "@nestjs/swagger";
 import { WorkshiftPaymentCategoryEntity } from "src/restaurants/workshift-payment-categories/entity/workshift-payment-category.entity";
 
 export class UpdateWorkshiftPaymentCategoryDto extends PartialType(
-  PickType(WorkshiftPaymentCategoryEntity, ["name", "description"]),
+  PickType(WorkshiftPaymentCategoryEntity, ["name", "description", "isActive"]),
 ) {}
