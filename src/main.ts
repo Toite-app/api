3a48440b3f08781d1e12f4c489712558866e0dd3
Yefrosynii Kolenko <contact@yefro.dev>
2025-03-25 16:51:16 +0200

    feat: run swagger only on dev

--- src/main.ts
+++ src/main.ts
@@ -13,31 +13,38 @@ async function bootstrap() {
 
   configApp(app);
 
-  const config = new DocumentBuilder()
-    .setTitle("Toite API")
-    .setDescription("The API part of the Toite project")
-    .setVersion("1.0.0 (just started)")
-    .addCookieAuth(AUTH_COOKIES.token, {
-      type: "apiKey",
-    })
-    .setContact("Yefrosynii", "https://www.yefro.dev/", "contact@yefro.dev")
-    .addTag("workers", "Get data about workers and manage them")
-    .addTag("auth", "Part of authentification for workers part of the system")
-    .addTag("restaurants", "Get data about restaurants and manage them")
-    .addTag("guests", "Get data about guests and manage them")
-    .build();
+  // Only setup Swagger in development
+  if (env.NODE_ENV !== "production") {
+    const config = new DocumentBuilder()
+      .setTitle("Toite API")
+      .setDescription("The API part of the Toite project")
+      .setVersion("1.0.0 (just started)")
+      .addCookieAuth(AUTH_COOKIES.token, {
+        type: "apiKey",
+      })
+      .setContact("Yefrosynii", "https://www.yefro.dev/", "contact@yefro.dev")
+      .addTag("workers", "Get data about workers and manage them")
+      .addTag("auth", "Part of authentification for workers part of the system")
+      .addTag("restaurants", "Get data about restaurants and manage them")
+      .addTag("guests", "Get data about guests and manage them")
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+    const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup("docs", app, document, {
-    customSiteTitle: "Toite API Docs",
-    customfavIcon:
-      "https://avatars.githubusercontent.com/u/157302718?s=200&v=4",
-    swaggerOptions: {},
-  });
+    SwaggerModule.setup("docs", app, document, {
+      customSiteTitle: "Toite API Docs",
+      customfavIcon:
+        "https://avatars.githubusercontent.com/u/157302718?s=200&v=4",
+      swaggerOptions: {},
+    });
+  }
 
   await app.listen(env.PORT);
 }
 
-patchNestJsSwagger();
+// Only patch Swagger in development
+if (env.NODE_ENV !== "production") {
+  patchNestJsSwagger();
+}
+
 bootstrap();
