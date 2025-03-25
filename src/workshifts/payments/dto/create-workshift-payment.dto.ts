4e8af2041925eea15ff435fab958dd3ed8a967a4
Yefrosynii Kolenko <contact@yefro.dev>
2025-03-25 23:33:06 +0200

    feat: take restaurant currency for workshift payments

--- src/workshifts/payments/dto/create-workshift-payment.dto.ts
+++ src/workshifts/payments/dto/create-workshift-payment.dto.ts
@@ -2,6 +2,6 @@ import { IntersectionType, PartialType, PickType } from "@nestjs/swagger";
 import { WorkshiftPaymentEntity } from "src/workshifts/payments/entity/workshift-payment.entity";
 
 export class CreateWorkshiftPaymentDto extends IntersectionType(
-  PickType(WorkshiftPaymentEntity, ["categoryId", "amount", "currency"]),
+  PickType(WorkshiftPaymentEntity, ["categoryId", "amount"]),
   PartialType(PickType(WorkshiftPaymentEntity, ["note"])),
 ) {}
