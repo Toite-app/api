4d427b29c452bb9460e8a2420bb0a3472bd6b003
Yefrosynii Kolenko <contact@yefro.dev>
2025-03-27 20:00:34 +0200

    feat: find one kitchener order endpoint

--- src/orders/@queue/index.ts
+++ src/orders/@queue/index.ts
@@ -3,5 +3,4 @@ export const ORDERS_QUEUE = "orders";
 export enum OrderQueueJobName {
   CRUD_UPDATE = "crud-update",
   DISH_CRUD_UPDATE = "dish-crud-update",
-  RECALCULATE_PRICES = "recalculate-prices",
 }
