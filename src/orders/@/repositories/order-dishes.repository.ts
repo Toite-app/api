1047383ca572f3e02b2389624fb2a70a7d9477b9
Yefrosynii Kolenko <contact@yefro.dev>
2025-04-04 07:44:03 +0300

    refactor: order discounts methods and repository

--- src/orders/@/repositories/order-dishes.repository.ts
+++ src/orders/@/repositories/order-dishes.repository.ts
@@ -1,6 +1,6 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, Logger } from "@nestjs/common";
 import { DrizzleTransaction, Schema } from "@postgress-db/drizzle.module";
-import { orderDishes } from "@postgress-db/schema/order-dishes";
+import { IOrderDish, orderDishes } from "@postgress-db/schema/order-dishes";
 import { plainToClass } from "class-transformer";
 import { eq } from "drizzle-orm";
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
@@ -15,6 +15,8 @@ export type OrderDishUpdatePayload = {
 
 @Injectable()
 export class OrderDishesRepository {
+  private readonly logger = new Logger(OrderDishesRepository.name);
+
   constructor(
     // Postgres connection
     @Inject(PG_CONNECTION)
@@ -36,6 +38,119 @@ export class OrderDishesRepository {
   }
 
   /**
+   * Validates payload
+   * @param payload
+   */
+  private _validatePayload(
+    payload: Omit<OrderDishUpdatePayload, "orderDishId">,
+  ) {
+    if (
+      (payload.discountPercent ||
+        payload.surchargePercent ||
+        payload.discountAmount ||
+        payload.surchargeAmount) &&
+      !payload.price
+    ) {
+      this.logger.error(
+        `Discount/surcharge is set but we don't have price to handle it correctly`,
+        {
+          payload,
+        },
+      );
+
+      throw new Error("price is required");
+    }
+  }
+
+  /**
+   * Calculates prices
+   * @param payload
+   * @returns
+   */
+  private _calculatePrices(
+    payload: Pick<
+      OrderDishUpdatePayload,
+      | "price"
+      | "discountAmount"
+      | "surchargeAmount"
+      | "discountPercent"
+      | "surchargePercent"
+    >,
+  ) {
+    const prices: Pick<
+      IOrderDish,
+      | "price"
+      | "discountAmount"
+      | "surchargeAmount"
+      | "finalPrice"
+      | "discountPercent"
+      | "surchargePercent"
+    > = {
+      price: "0",
+      discountAmount: "0",
+      surchargeAmount: "0",
+      discountPercent: "0",
+      surchargePercent: "0",
+      finalPrice: "0",
+    };
+
+    if (!payload.price) return {};
+
+    const {
+      price,
+      discountAmount,
+      // surchargeAmount,
+      discountPercent,
+      // surchargePercent,
+    } = payload;
+
+    prices.price = price;
+
+    if (discountAmount && !discountPercent) {
+      prices.discountAmount = discountAmount;
+      prices.discountPercent = String(
+        (Number(discountAmount) / Number(price)) * 100,
+      );
+    } else if (discountPercent && !discountAmount) {
+      prices.discountPercent = discountPercent;
+      prices.discountAmount = String(
+        (Number(price) * Number(discountPercent)) / 100,
+      );
+    } else if (discountAmount && discountPercent) {
+      // Check if provided values are correct
+      const _expectedDiscountAmount =
+        (Number(price) * Number(discountPercent)) / 100;
+      const _expectedDiscountPercent =
+        (Number(discountAmount) / Number(price)) * 100;
+
+      if (
+        _expectedDiscountAmount !== Number(discountAmount) ||
+        _expectedDiscountPercent !== Number(discountPercent)
+      ) {
+        // throw new Error("discountAmount is incorrect");
+        this.logger.warn(`Discount amount differs`, {
+          expected: {
+            discountAmount: _expectedDiscountAmount,
+            discountPercent: _expectedDiscountPercent,
+          },
+          provided: {
+            discountAmount,
+            discountPercent,
+          },
+        });
+      }
+    }
+
+    prices.finalPrice = String(
+      Number(prices.price) -
+        Number(prices.discountAmount) +
+        Number(prices.surchargeAmount),
+    );
+
+    return prices;
+  }
+
+  /**
    * Creates new order dish. Without any checks, just data handling (including snapshot)
    * @param payload - Order dish data
    * @param opts - Transaction options
@@ -51,10 +166,15 @@ export class OrderDishesRepository {
     const tx = opts?.tx ?? this.pg;
 
     const result = await tx.transaction(async (tx) => {
+      this._validatePayload(payload);
+
       // Insert new data to database
       const [orderDish] = await tx
         .insert(orderDishes)
-        .values(payload)
+        .values({
+          ...payload,
+          ...this._calculatePrices(payload),
+        })
         .returning();
 
       // Calculate order totals price
@@ -96,10 +216,15 @@ export class OrderDishesRepository {
     const tx = opts?.tx ?? this.pg;
 
     const result = await tx.transaction(async (tx) => {
+      this._validatePayload(payload);
+
       // Update order dish
       const [orderDish] = await tx
         .update(orderDishes)
-        .set(payload)
+        .set({
+          ...payload,
+          ...this._calculatePrices(payload),
+        })
         .where(eq(orderDishes.id, orderDishId))
         .returning();
 
